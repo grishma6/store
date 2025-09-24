8b01abec7e3f7bb8e3e71e06abbed08846794ee7
grishma6 <gxg59150@ucmo.edu>
2025-09-24 14:25:45 -0500

    Configuring beans using code

--- src/main/java/com/example/store/HomeController.java
+++ src/main/java/com/example/store/HomeController.java
@@ -13,7 +13,6 @@ public class HomeController {
     @RequestMapping("/")
     public String index(){
         return "appName: " + appName;
-
     }
 
 }
--- src/main/java/com/example/store/OrderService.java
+++ src/main/java/com/example/store/OrderService.java
@@ -3,10 +3,10 @@ package com.example.store;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class OrderService {
     private PaymentService paymentService;
-    public OrderService(@Qualifier("stripe") PaymentService paymentService) {
+    public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
 }
 
